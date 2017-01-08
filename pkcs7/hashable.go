77cdc19b8129d4f803a98e7e7503d4d90cd51c16
Yannic Bonenberger <contact@yannic-bonenberger.com>
2017-01-08 15:43:09 +0100

    Add interface Hashable

---
+++ pkcs7/hashable.go
@@ -0,0 +1,42 @@
+package pkcs7
+
+import (
+	"crypto/sha256"
+	"io"
+)
+
+type Hashable interface {
+	Hash() ([]byte, error)
+}
+
+type hashableReader struct {
+	r io.Reader
+}
+
+func (h *hashableReader) Hash() ([]byte, error) {
+	hash := sha256.New()
+
+	if _, err := io.Copy(hash, h.r); err != nil {
+		return nil, err
+	}
+
+	return hash.Sum(nil), nil
+}
+
+func NewHashableReader(r io.Reader) Hashable {
+	return &hashableReader{r: r}
+}
+
+type hashableBytes struct {
+	b []byte
+}
+
+func (h *hashableBytes) Hash() ([]byte, error) {
+	hash := sha256.New()
+	hash.Write(h.b)
+	return hash.Sum(nil), nil
+}
+
+func NewHashableBytes(b []byte) Hashable {
+	return &hashableBytes{b: b}
+}
